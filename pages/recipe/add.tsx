9cb4c619ca1bc0557e9f4d3ab4bf265fd07d021b
yeudaborodyanski <yehudab@hadran.net>
2024-09-02 00:31:36 +0300

    add screen

--- pages/recipe/add.tsx
+++ pages/recipe/add.tsx
@@ -1,4 +1,4 @@
-import {UploadDropzone} from "@/src/components/uploadthing";
+import {UploadButton} from "@/src/components/uploadthing";
 import {useSession} from "next-auth/react";
 import {useState} from "react";
 
@@ -9,24 +9,25 @@ export default function AddRecipe() {
 
     if (session) {
         return (
-            <div className={"flex flex-col gap-2 rounded-xl bg-pastelLavender-50 p-4 m-6"}>
-                <h1>Add a recipe</h1>
+            <div className={"flex flex-col gap-4 rounded-xl p-2"}>
+                <h1 className={"font-bold text-xl tracking-wide text-center text-blue-mint-text"}>+ Add a recipe</h1>
                 <form action={"/api/recipe"} method={"PUT"}
                       className={"flex flex-col gap-2"}>
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" name="title" required/>
-                    <label htmlFor="description">Description</label>
-                    <textarea id="description" name="description" required/>
-                    <UploadDropzone
+                    <input className={"w-full border-2 rounded-xl border-blue-mint-dark p-2"} type="text" id="title"
+                           name="title" required placeholder={"Name..."}/>
+                    <textarea className={"w-full border-2 rounded-xl border-blue-mint-dark p-2"} id="description"
+                              name="description" placeholder={"Description..."} required/>
+                    <UploadButton
                         endpoint={"imageUploader"}
+                        className={"ut-button:bg-blue-mint-lighter/5 ut-button:border--blue-mint-light ut-button:border-4 ut-button:rounded-full ut-label:text-blue-mint-text ut-button:text-blue-mint-text ut-button:shadow-lg ut-button:w-full"}
                         onClientUploadComplete={(res) => {
                             setImage(res[0].url);
                         }}
                     />
                     <input type="hidden" name="image" value={image}/>
                     <button type="submit"
-                            className={"bg-pastelLavender-800 text-pastelPurple-100 p-2 rounded-full shadow-lg"}>
-                        Add recipe
+                            className={"bg-blue-mint-lighter mt-8 w-full text-blue-mint-text p-2 rounded-full shadow-lg"}>
+                        Save
                     </button>
                 </form>
             </div>
